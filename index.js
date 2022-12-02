db4a232ce7612e8c1c09db4c713b23bb6a640403
BBBBQ <94879501+BBBBBBQ@users.noreply.github.com>
2022-12-02 16:36:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import axios from 'axios';
+//import express from "express";
+//import axios from 'axios';
 
 
 // const { clientId, guildId, token, publicKey } = require('./config.json');
